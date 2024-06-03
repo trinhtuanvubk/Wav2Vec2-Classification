85fd4927a623360f1c5415bfe23c142e485bffb2
TuanVuTrinh <vutuantrinh2000@gmail.com>
2024-06-03 14:30:00 +0700

    infer

--- infer.py
+++ infer.py
@@ -6,28 +6,35 @@ from transformers import AutoConfig, Wav2Vec2FeatureExtractor
 from train import Wav2Vec2ClasificationTrainer
 from model import Wav2Vec2ForSpeechClassification
 import librosa
-device = torch.device("cuda")
 
-print()
+def infer(model_path = "/content/Wav2Vec2-Classification/checkpoints/checkpoint-40",
+          audio_filepath = "data/sounds/burger/burger_1_01.wav"):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model_name_or_path = "/content/Wav2Vec2-Classification/checkpoints/checkpoint-40"
-config = AutoConfig.from_pretrained(model_name_or_path)
-feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
-model = Wav2Vec2ForSpeechClassification.from_pretrained(model_name_or_path).to(device)
+    config = AutoConfig.from_pretrained(model_path)
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_path)
+    model = Wav2Vec2ForSpeechClassification.from_pretrained(model_path).to(device)
 
-id2label = Wav2Vec2ClasificationTrainer("data/train.csv", "data/train.csv").config.id2label
+    # id2label = Wav2Vec2ClasificationTrainer("data/train.csv", "data/train.csv").config.id2label
+    id2label = config.id2label
 
-speech_array, sampling_rate = torchaudio.load("data/sounds/burger/burger_1_01.wav")
-speech_array = speech_array.squeeze().numpy()
-speech_array = librosa.resample(np.asarray(speech_array), sampling_rate, feature_extractor.sampling_rate)
+    speech_array, sampling_rate = torchaudio.load(audio_filepath)
+    speech_array = speech_array.squeeze().numpy()
+    speech_array = librosa.resample(np.asarray(speech_array), sampling_rate, feature_extractor.sampling_rate)
 
-features = feature_extractor(speech_array, sampling_rate=feature_extractor.sampling_rate, return_tensors="pt", padding=True)
+    features = feature_extractor(speech_array, sampling_rate=feature_extractor.sampling_rate, return_tensors="pt", padding=True)
 
-input_values = features.input_values.to(device)
+    input_values = features.input_values.to(device)
 
-with torch.no_grad():
-    logits = model(input_values).logits
+    with torch.no_grad():
+        logits = model(input_values).logits
 
-pred_ids = torch.argmax(logits, dim=-1).detach().cpu().numpy()
-print(pred_ids)
-print(id2label[pred_ids[0]])
+    pred_ids = torch.argmax(logits, dim=-1).detach().cpu().numpy()
+    print(pred_ids)
+    print(id2label[pred_ids[0]])
+
+
+if __name__=="__main__":
+    model_path = "/content/Wav2Vec2-Classification/checkpoints/checkpoint-40"
+    audio_filepath = "data/sounds/burger/burger_1_01.wav"
+    infer(model_path, audio_filepath)
